bdb8b70892c266a314b285acde81304e2adb18e2
shortnickname <selimasha@mail.ru>
2022-04-20 16:04:13 +0700

    г
    г

--- source/js/menu-management.js
+++ source/js/menu-management.js
@@ -1,11 +1,11 @@
-const burgerButton = document.querySelector(".main-nav__toggle");
+const burgerButton = document.querySelector(".main-nav__burger-toggle");
 const mainNav = document.querySelector(".main-nav");
 
 mainNav.classList.add("main-nav--closed");
 
 burgerButton.onclick = function()
-{mainNav.classList.toggle("main-nav--closed");
-mainNav.classList.toggle("main-nav--opened");
+{mainNav.classList.toggle("main-nav--opened");
+mainNav.classList.toggle("main-nav--closed");
 
 };
 
